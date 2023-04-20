328268cca8e28d53436bc94dfee778a2e3c7bd7f
WeinbergerPeti <weinbergerpeti@gmail.com>
2023-04-20 12:15:07 +0200

    oszlop hozzáadása

--- database/migrations/2023_04_20_101151_create_proba_table.php
+++ database/migrations/2023_04_20_101151_create_proba_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('proba', function (Blueprint $table) {
             $table->id();
+            $table->string();
             $table->timestamps();
         });
     }
